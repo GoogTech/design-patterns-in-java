b038a7b719127a1115ffa82ed94c4f945cc3b9f3
HuangYuhui <Gentleman_0109@outlook.com>
2019-09-26 13:57:49 +0800

    :tada: init the project

---
+++ design-patterns/src/main/java/pers/huangyuhui/App.java
@@ -0,0 +1,10 @@
+package pers.huangyuhui;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ design-patterns/src/test/java/pers/huangyuhui/AppTest.java
@@ -0,0 +1,18 @@
+package pers.huangyuhui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test1 :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
